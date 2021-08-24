bff4bfa9e03135d06fa4c6b735295b6d8091b66c
Rishi <61271406+rishi7pillai@users.noreply.github.com>
2021-08-24 09:57:20 +0530

    Add files via upload

--- SendMail.py
+++ SendMail.py
@@ -1,12 +1,12 @@
 import smtplib
 Server=smtplib.SMTP('smtp.gmail.com',587)
 Server.starttls()
-Server.login("Hrushikeshpollai777@gmail.com","9090444025")
+Server.login("Hrushikeshpollai777@gmail.com","pss")
 subject="Sending Email Using Python"
 body="this is a text mail for testing purpose !!!"
 message="subject:{}\n\n{}".format(subject,body)
 print(message)
-listOfAddress=["rishipollai@gmail.com","nikhyheart@gmail.com"]
+listOfAddress=["rishipollai@gmail.com"]
 Server.sendmail("Hrushikeshpollai777@gmail.com",listOfAddress,message)
 print("email send succesfully") #Just for confirmation
 Server.quit()
